a189bcdbd6810b6d2391f7a8d2eecf5132927d42
ZulNs <zul.nasibu@gmail.com>
2018-12-29 16:21:35 +0800

    Add files via upload

---
+++ zCalc/AssemblyInfo.vb
@@ -0,0 +1,28 @@
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Scientific Calculator")> 
+<Assembly: AssemblyDescription("Keyword: AK51")> 
+<Assembly: AssemblyCompany("Personal")> 
+<Assembly: AssemblyProduct("zCalc .NET")> 
+<Assembly: AssemblyCopyright("Iskandar Z. Nasibu, Gorontalo, April 2007.")> 
+<Assembly: AssemblyTrademark("zCalc")> 
+<Assembly: CLSCompliant(True)> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0")> 
